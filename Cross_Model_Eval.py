963213c43cc067435501cfaa99114cfc6d6e31cb
Douhuanmin <dj4569103@gmail.com>
2023-09-21 19:30:57 +0800

    2023-9-21

---
+++ Cross_Model_Eval.py
@@ -0,0 +1,172 @@
+from Mat_loader import Mat_index
+
+from Utils.evaluate import *
+import scipy.io as scio
+from loguru import logger
+from Utils import PR_Curve as PR
+from Utils.NDCG import cal_NDCG
+import glob
+class Cross_Mat_index(Mat_index):
+    def __init__(self, dataset , filepath,i2t,t2i ,color='blue' ,
+                 modelname='DSH'
+                 ,K=[0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
+                 ):
+        # super().__init__()
+        print('mat文件的命名规则是{binary_bits}-{dataset}-{modelname}')
+        self.modelname = modelname
+        self.dataset = dataset
+        self.filepath = filepath
+        self.color = color
+        self.bits = 16
+        self.K = K
+        self.baseroot = './Result'
+        self.i2t = i2t
+        self.t2i = t2i
+
+    def load_mat(self): # 迭代器
+        test = 'i2t' if self.i2t else 't2i'
+        file_list = glob.glob(os.path.join(self.filepath,f'*{self.dataset}*{test}*.mat'))
+        print(file_list)
+        file_basename = [os.path.splitext(os.path.basename(f))[0] for f in file_list]
+        # self.basename = file_basename
+        mat_dict = {i : scio.loadmat(file_list[i]) for i in range(len(file_list))}
+        self.name_dict = {i :  file_basename[i] for i in range(len(file_basename))}
+        for i in range(len(file_list)):
+            print(f'Processing the {i + 1} th file')
+
+            # if 'i2t' in file_basename[i]:
+            #     self.i2t = True
+
+            qib , ql , qtb = mat_dict[i]['q_img'] , mat_dict[i]['q_l'] ,mat_dict[i]['q_txt']
+            rib , rl , rtb= mat_dict[i]['r_img'] , mat_dict[i]['r_l'] , mat_dict[i]['r_txt']
+            yield qib,rib,qtb,rtb,ql,rl
+
+    def prcurve(self):
+        iter_mat = iter(self.load_mat())
+        while True :
+            try:
+                qib,rib,qtb,rtb,ql,rl  = next(iter_mat)
+
+                self.bits = qib.shape[1]
+                qib = torch.from_numpy(qib)
+                qtb = torch.from_numpy(qtb)
+                rib = torch.from_numpy(rib)
+                rtb = torch.from_numpy(rtb)
+                ql = torch.from_numpy(ql)
+                rl = torch.from_numpy(rl)
+                if self.i2t :
+                    p, r = PR.pr_curve(qib, rtb, ql, rl)
+                    save_csv('saved_I2T_index_pr', p, r, f'{self.bits}_{self.dataset}_Precison',
+                             f'{self.bits}_{self.dataset}_Recall')
+
+                if self.t2i  :
+                    p, r = PR.pr_curve(qtb, rib, ql, rl)
+                    save_csv('saved_T2I_index_pr', p, r, f'{self.bits}_{self.dataset}_Precison',
+                             f'{self.bits}_{self.dataset}_Recall')
+
+            except StopIteration :
+                print("Finish computing PRcurve")
+                break
+    def topK_recall(self):
+        iter_mat = iter(self.load_mat())
+        while True:
+            try:
+                qib, rib, qtb, rtb, ql, rl = next(iter_mat)
+                self.bits = qib.shape[1]
+                ql = np.squeeze(ql)
+                rl = np.squeeze(rl)
+                recallK = []
+                if self.i2t :
+                    for i in self.K:
+                        print(i)
+                        _, a, _ = mean_average_precision_normal_optimized_topK(rtb, rl, qib, ql, i)
+                        print(f'{self.bits}_top{i}_recall is {a}')
+                        recallK.append(a)
+                    save_csv('saved_I2T_TopK_recall', self.K, recallK, 'K',
+                             f'{self.bits}_{self.dataset}_Recall')
+
+                else :
+                    for i in self.K:
+                        print(i)
+                        _, a, _ = mean_average_precision_normal_optimized_topK(rib, rl, qtb, ql, i)
+                        print(f'{self.bits}_top{i}_recall is {a}')
+                        recallK.append(a)
+                    save_csv('saved_T2I_TopK_recall', self.K, recallK, 'K',
+                             f'{self.bits}_{self.dataset}_Recall')
+
+            except StopIteration:
+                break
+
+    def topK_precision(self,num=10):
+        iter_mat = iter(self.load_mat())
+        while True:
+            try:
+                qib, rib, qtb, rtb, ql, rl = next(iter_mat)
+                self.bits = qib.shape[1]
+                ql = np.squeeze(ql)
+                rl = np.squeeze(rl)
+                K = self.K
+                P = []
+                if self.i2t :
+                    for i in self.K:
+                        print(i)
+                        a, _, _ = mean_average_precision_normal_optimized_topK(rtb, rl, qib, ql, i)
+                        print(f'{self.bits}_top{i}_precision is {a}')
+                        P.append(a)
+                    save_csv('saved_I2T_topK_precision', K, P, 'K',
+                             f'{self.bits}_{self.dataset}_Precision')
+                else :
+                    for i in self.K:
+                        print(i)
+                        a, _, _ = mean_average_precision_normal_optimized_topK(rib, rl, qtb, ql, i)
+                        print(f'{self.bits}_top{i}_precision is {a}')
+                        P.append(a)
+                    save_csv('saved_T2I_topK_precision', K, P, 'K', f'{self.bits}_{self.dataset}_Precision')
+            except StopIteration:
+                break
+    def NDCG_1000(self):
+        "numpy_array"
+        logger.add(f'the NDCG of {self.dataset} with {self.modelname}', format="{time} {level} {message}", level="INFO")
+        iter_mat = iter(self.load_mat())
+        while True:
+            try:
+                qib, rib, qtb, rtb, ql, rl = next(iter_mat)
+                self.bits = qib.shape[1]
+                ql = np.squeeze(ql)
+                rl = np.squeeze(rl)
+                if self.i2t:
+                    logger.info(f'{self.bits}_{self.dataset}_I2T_NDCG : {cal_NDCG(qib, rtb, ql, rl, what=0, k=1000)}')
+                else :
+                    logger.info(f'{self.bits}_{self.dataset}_T2I_NDCG : {cal_NDCG(qtb, rib, ql, rl, what=0, k=1000)}')
+            except StopIteration:
+                break
+
+    def phamming2(self , num=10):
+        iter_mat = iter(self.load_mat())
+        ph2 = np.array([])
+        recall_ph2 = np.array([])
+        Map = np.array([])
+        while True:
+            try:
+                qib, rib, qtb, rtb, ql, rl = next(iter_mat)
+                self.bits = qib.shape[1]
+                print(f'Calculating {self.bits} ......')
+                if self.i2t:
+                    precision, recall, map = get_precision_recall_by_Hamming_Radius(database_output=rtb,
+                                                                                    database_labels=rl,
+                                                                                    query_output=qib,
+                                                                                    query_labels=ql)
+                else :
+                    precision, recall, map = get_precision_recall_by_Hamming_Radius(database_output=rib,
+                                                                                    database_labels=rl,
+                                                                                    query_output=qtb,
+                                                                                    query_labels=ql)
+                ph2 = np.append(ph2 , precision)
+
+            except StopIteration:
+                if self.i2t:
+                    np.savetxt(f'./Result/PH@2_I2T_{self.bits}_{self.dataset}_{self.modelname}', ph2, delimiter=',')
+                else :
+                    np.savetxt(f'./Result/PH@2_T2I_{self.bits}_{self.dataset}_{self.modelname}', ph2, delimiter=',')
+                print('Finish computing PH@2')
+                break
--- run.py
+++ run.py
@@ -1,20 +1,39 @@
 from Mat_loader import *
 import loguru
 import argparse
+from Cross_Model_Eval import *
 def main():
-    print('mat文件的命名规则是{binary_bits}-{dataset}-{modelname}')
+    print('mat文件的命名规则是{binary_bits}-{dataset}-{modelname}.mat ')
     parser = argparse.ArgumentParser(description='Get Mat index')
-    parser.add_argument('--file',default='./MAT',help='path to mat')
-    parser.add_argument('--dataset',default='cifar10',help='name of dataset')
-    parser.add_argument('--modelname',default='VIT',help='modelname')
-    parser.add_argument('--label',default=10,help='number of the class')
+    parser.add_argument('--file',default='./MAT/Flickr25k',help='path to mat')
+    parser.add_argument('--dataset',default='flickr25k',help='name of dataset')
+    parser.add_argument('--modelname',default='ours',help='modelname')
+    parser.add_argument('--label',default=24,help='number of the class')
+
+    parser.add_argument('--ifCrossModel',default=True,help='是否是跨模态')
+    parser.add_argument('--if_i2t',default=True,help='image to text or text to image')
     args = parser.parse_args()
-    get_index = Mat_index(filepath=args.file,dataset=args.dataset,modelname=args.modelname)
-    get_index.topK_recall()
-    get_index.prcurve()
-    get_index.topK_precision(num=args.label)
-    get_index.phamming2(num=args.label)
-    get_index.NDCG_1000()
+    if args.ifCrossModel :
+        get_index = Cross_Mat_index(filepath=args.file,
+                                    dataset=args.dataset,
+                                    modelname=args.modelname,
+                                    i2t=args.if_i2t,
+                                    t2i=(args.if_i2t == False))
+        get_index.topK_recall()
+        get_index.prcurve()
+        get_index.topK_precision(num=args.label)
+        get_index.phamming2(num=args.label)
+        get_index.NDCG_1000()
+    else :
+        get_index = Mat_index(filepath=args.file,
+                              dataset=args.dataset,
+                              modelname=args.modelname)
+        get_index.topK_recall()
+        get_index.prcurve()
+        get_index.topK_precision(num=args.label)
+        get_index.phamming2(num=args.label)
+        get_index.NDCG_1000()
+
 if __name__ == '__main__':
     main()
 
